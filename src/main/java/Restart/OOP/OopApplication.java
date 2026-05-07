8757552faab71b090ab98cb3fa74b5776edf32ed
Yugal Patel <patelyug7447@gmail.com>
2026-05-07 22:18:19 +0100

    Date 07/05/2026

--- src/main/java/Restart/OOP/OopApplication.java
+++ src/main/java/Restart/OOP/OopApplication.java
@@ -2,12 +2,22 @@ package Restart.OOP;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(
+		exclude = {
+				DataSourceAutoConfiguration.class,
+				HibernateJpaAutoConfiguration.class
+		}
+)
+
 public class OopApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OopApplication.class, args);
 	}
+
+
 
 }
